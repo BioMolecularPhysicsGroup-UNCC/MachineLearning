7d658bb5146a59328cc0c9409c3f2333ac491810
johnnyp117 <35807919+johnnyp117@users.noreply.github.com>
2021-01-19 11:32:21 -0500

    Delete getRMSIPsequence.m

--- SPLOC/getRMSIPsequence.m
+++
@@ -1,9 +0,0 @@
-function rmsipSequence = getRMSIPsequence(U,V)
-% yeilds sequence of root mean squared inner products as DIM(U) is scanned 
-m = length(U);
-rmsipSequence = zeros(1,m);
-   for k=1:m
-   subset = 1:k;
-   rmsipSequence(k) = getRMSIP(U(:,subset),V);
-   end
-end 
